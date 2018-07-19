275077764853bce5251e407c32b2355a568dc32c
Robert Snyder <r.snyder49@gmail.com>
2018-07-19 19:59:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,16 @@
 
 def fizzbuzz(int)
   if int % 3 === 0 && int % 5 === 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   
   elsif int % 3 === 0
-    puts "Fizz"
+    return "Fizz"
   
   elsif int % 5 === 0 
-    puts "Buzz"
+    return "Buzz"
   
   else
   return nil 
-   end 
+  
+  end 
 end 
